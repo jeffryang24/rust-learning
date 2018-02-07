e1c641fb9cea29bb6676218e93528f5da64ada41
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2018-02-07 20:11:10 +0700

    -> rust day 1

---
+++ comment.rs
@@ -0,0 +1,12 @@
+/*
+ * Comment Multiline
+ */
+
+// Comment single
+
+/// Documentation
+
+fn main() {
+    let x = 5 * 5;
+    println!("x: {}", x);
+}
---
+++ print.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("days: {}", 31);
+    println!("{0}, this is {1} and {1} this is {0}", "Alice", "Bob");
+    println!("{subject} {verb} {object}", subject = "I", verb = "Like", object = "an Apple");
+    println!("{} of {:b}", 1, 2);
+    println!("{no:>width$}", no = 1, width = 5);
+    println!("{no:>0width$}", no = 1, width = 6);
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    #[allow(dead_code)]
+    struct Structure(i32);
+}
