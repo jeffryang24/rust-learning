aed89bb74b72f8beb785984eb8d2c843d0da9d7c
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2018-02-13 23:02:34 +0700

    -> new from Banas

---
+++ Derek Banas/1231.rs
@@ -0,0 +1,23 @@
+use std::{i8, i16, i32, i64, u8, u16, u32, u64, isize, usize, f32, f64};
+
+use std::io::stdin;
+
+fn main() {
+    // println!("5 + 4 = {}", 5 + 4);
+    // println!("5 - 4 = {}", 5 - 4);
+    // println!("5 * 4 = {}", 5 * 4);
+    // println!("5 / 4 = {}", 5 / 4);
+    // println!("5 % 4 = {}", 5 % 4);
+
+    let mut neg_4 = -4i32;
+
+    println!("abs(-4) = {}", neg_4.abs());
+    println!("4 ^ 6 = {}", 4i32.pow(6));
+    println!("sqrt 9 = {}", 9f32.sqrt());
+    println!("cbrt 27 = {}", 27f32.cbrt());
+    println!("round 1.45 = {}", 1.45f32.round());
+    println!("ceiling 1.45 = {}", 1.45f32.ceil());
+    println!("e ^ 2 = {}", 2f32.exp());
+    println!("log(2) = {}", 2f32.ln());
+    println!("log10(2) = {}", 2f32.log10());
+}
